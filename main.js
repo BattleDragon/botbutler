3dd5cc670536e83ac0790bd5b558b727d6b0b2e6
BattleDragon <comedianchameleon@gmail.com>
2018-08-24 18:59:28 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
+});
+
+client.on('message', msg => "hi");
+
+client.login(process.env.token);
