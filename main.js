50996b5f38f1942065f1fa4474bed8d30b44618b
BattleDragon <comedianchameleon@gmail.com>
2018-08-25 14:14:48 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+client.login(process.env.token);
 
 client.on('ready', () => {
     client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
 });
 
-client.on('message', msg => message.reply('Hey, I\'m a reply!'));
+client.on('voiceStateUpdate', function(oldMemberState, newMemberState){
+    if ((oldMemberState.voiceChannel == null) && (newMemberState.voiceChannel != oldMemberState.voiceChannel)){
+        // If the member was previously not in a voice channel, and now is in one . . .
+        message.member.addRole('483014771818364948');
+    } else if ((oldMemberState.voiceChannel != null) && (newMemberState.voiceChannel == null)){
+        // If the user was previously in a channel and is now not in one . . .
+        message.member.removeRole('483014771818364948');
+    }
+});
 
-client.login(process.env.token);
