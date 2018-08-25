afa87ae97a33363c3a03ec94a1130b63b0ef94c1
BattleDragon <comedianchameleon@gmail.com>
2018-08-25 15:09:28 -0700

    Update main.js

--- main.js
+++ main.js
@@ -9,10 +9,10 @@ client.on('ready', () => {
 client.on('voiceStateUpdate', function(oldMemberState, newMemberState){
     if ((oldMemberState.voiceChannel == null) && (newMemberState.voiceChannel != oldMemberState.voiceChannel)){
         // If the member was previously not in a voice channel, and now is in one . . .
-        message.member.addRole('483014771818364948');
+        newMemberState.addRole('483014771818364948');
     } else if ((oldMemberState.voiceChannel != null) && (newMemberState.voiceChannel == null)){
         // If the user was previously in a channel and is now not in one . . .
-        message.member.removeRole('483014771818364948');
+        newMemberState.removeRole('483014771818364948');
     }
 });
 
