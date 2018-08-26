a23f760f9ccbf39ac49edf412fe0af7ec57103f8
BattleDragon <comedianchameleon@gmail.com>
2018-08-26 14:14:58 -0700

    Update main.js

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ function evalCmd(message, code) {
     try {
         let evaled = eval(code);
         if (typeof evaled !== "string")
-//            evaled = util.inspect(evaled);
+            evaled = util.inspect(evaled);
             message.channel.send(clean(evaled), {code:"xl"});
     } catch (err) {
         message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
