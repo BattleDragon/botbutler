00955b8d0af03da3618eb071cadfa21e591bcd39
BattleDragon <comedianchameleon@gmail.com>
2018-08-26 05:28:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const util = require('util');
 const config = require('config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 client.login(process.env.token);
 
 client.on('ready', () => {
@@ -54,3 +53,26 @@ client.on("message", async message => {
         }
     }
 });
+
+function evalCmd(message, code) {
+    if(message.author.id !== config.owner) return;
+    try {
+        let evaled = eval(code);
+        if (typeof evaled !== "string")
+            evaled = util.inspect(evaled);
+            message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+}
+
+function clean(text) {
+    if (typeof(text) !== 'string') {
+        text = util.inspect(text, { depth: 0 });
+    }
+    text = text
+        .replace(/`/g, '`' + String.fromCharCode(8203))
+        .replace(/@/g, '@' + String.fromCharCode(8203))
+        .replace(config.token, 'mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0') //Don't let it post your token
+    return text;
+}
