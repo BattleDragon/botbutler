2735b5f6a618d7ef5cb49e78eb90c4fbd45f64fb
BattleDragon <comedianchameleon@gmail.com>
2018-08-26 14:31:22 -0700

    Update main.js

--- main.js
+++ main.js
@@ -57,22 +57,18 @@ function evalCmd(message, code) {
     if(message.author.id !== config.owner) return;
     try {
         let evaled = eval(code);
-/*        if (typeof evaled !== "string")
-            evaled = util.inspect(evaled);
-            message.channel.send(clean(evaled), {code:"xl"});*/
     } catch (err) {
-        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-        message.channel.send('hi');
+        message.channel.send(`\`\`\`xl\n${clean(err)}\n\`\`\``);
     }
 }
 
 function clean(text) {
-    if (typeof(text) !== 'string') {
+/*    if (typeof(text) !== 'string') {
         text = util.inspect(text, { depth: 0 });
-    }
+    }*/
     text = text
         .replace(/`/g, '`' + String.fromCharCode(8203))
         .replace(/@/g, '@' + String.fromCharCode(8203))
-        .replace(process.env.token, 'mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0') //Don't let it post your token
+        .replace(process.env.token, 'TOKEN') //Don't let it post your token
     return text;
 }
