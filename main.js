1e912dd2fc0b8ef26042291ae52cad25ac912814
BattleDragon <comedianchameleon@gmail.com>
2018-08-26 15:44:44 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ global.config = require('./config.json');
 global.Discord = require('discord.js');
 global.client = new Discord.Client();
 global.commands = require('commands.js');
-global.events = require('events.js);
+global.events = require('events.js');
 client.login(process.env.token);
