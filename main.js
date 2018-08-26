d3f2e14048e618c042defe48531e41470e418952
BattleDragon <comedianchameleon@gmail.com>
2018-08-26 03:48:07 -0700

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ client.on("message", async message => {
         else if (cmd === 'ping') {
             message.channel.send('pong');   
         }
-        else if(cmd === "purge") {
+        else if (cmd === 'purge') {
         // This command removes all messages from all users in the channel, up to 100.
         
         // get the delete count, as an actual number.
@@ -46,13 +46,12 @@ client.on("message", async message => {
         // Ooooh nice, combined conditions. <3
         if(!deleteCount || deleteCount < 2 || deleteCount > 100)
             return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
+
         // So we get our messages, and delete them. Simple enough, right?
         const fetched = await message.channel.fetchMessages({limit: deleteCount});
         message.channel.bulkDelete(fetched)
             .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-  }
-
+        }
         else { // if the command doesn't match anything you can say something or just ignore it
             message.channel.send(`I don't know what command that is.`);
             return;
