40bc7efb5a85334574e4fe781866d0194938132b
BattleDragon <comedianchameleon@gmail.com>
2018-08-26 14:13:59 -0700

    Update main.js

--- main.js
+++ main.js
@@ -57,9 +57,9 @@ function evalCmd(message, code) {
     if(message.author.id !== config.owner) return;
     try {
         let evaled = eval(code);
-/*        if (typeof evaled !== "string")
-            evaled = util.inspect(evaled);
-            message.channel.send(clean(evaled), {code:"xl"});*/
+        if (typeof evaled !== "string")
+//            evaled = util.inspect(evaled);
+            message.channel.send(clean(evaled), {code:"xl"});
     } catch (err) {
         message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
     }
