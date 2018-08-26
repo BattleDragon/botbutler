1b890527bbc77187752788d5d21d84f9ec7a89b6
BattleDragon <comedianchameleon@gmail.com>
2018-08-26 14:08:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -57,18 +57,18 @@ function evalCmd(message, code) {
     if(message.author.id !== config.owner) return;
     try {
         let evaled = eval(code);
-        if (typeof evaled !== "string")
+/*        if (typeof evaled !== "string")
             evaled = util.inspect(evaled);
-            message.channel.send(clean(evaled)/*, {code:"xl"}*/);
+            message.channel.send(clean(evaled), {code:"xl"});*/
     } catch (err) {
         message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
     }
 }
 
 function clean(text) {
-    if (typeof(text) !== 'string') {
-        text = util.inspect(text/*, { depth: 0 }*/);
-    }
+/*    if (typeof(text) !== 'string') {
+        text = util.inspect(text, { depth: 0 });
+    }*/
     text = text
         .replace(/`/g, '`' + String.fromCharCode(8203))
         .replace(/@/g, '@' + String.fromCharCode(8203))
